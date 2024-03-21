82aa79da002ee492773c2fc4edb8161cd094dc7d
(es)최경수 <es-kyeongsoo@hybecorp.com>
2024-03-21 16:23:11 +0900

    README.org, lsp-mode added

--- windows/.emacs.d/init.el
+++ windows/.emacs.d/init.el
@@ -12,11 +12,14 @@
 (setq inhibit-startup-message t)
 (setq visible-bell t)
 
+(setq scroll-step 1)
+(setq scroll-margin 8)
+
 (defvar penguin/default-font-size 110)
 (defvar penguin/default-variable-font-size 110)
 (defvar penguin/frame-transparency '(90 . 90))
-(defvar penguin/home "C:/Users/es-kyeongsoo" "My home dir")
-(defvar penguin/projects (concat penguin/home "/Projects") "My Projects Directory")
+(defvar penguin/home "C:\\Users\\es-kyeongsoo" "My home dir")
+(defvar penguin/projects (concat penguin/home "\\Projects") "My Projects Directory")
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
@@ -294,3 +297,106 @@
 (use-package counsel-projectile
   :after projectile
   :config (counsel-projectile-mode))
+
+
+(use-package dashboard
+  :ensure t 
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "Emacs Is More Than A Text Editor!")
+  (setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
+  ;;(setq dashboard-startup-banner "~/.config/emacs/images/dtmacs-logo.png")  ;; use custom image as banner
+  (setq dashboard-center-content nil) ;; set to 't' for centered content
+  (setq dashboard-items '((recents . 5)
+                          (agenda . 5 )
+                          (bookmarks . 3)
+                          (projects . 3)
+                          (registers . 3)))
+  :custom 
+  (dashboard-modify-heading-icons '((recents . "file-text")
+									(bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+
+
+(defun penguin/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . penguin/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-l")  ;; Or 'C-c l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+
+(use-package lsp-treemacs
+  :after lsp)
+
+
+(use-package lsp-ivy
+  :after lsp)
+
+
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+  :commands dap-debug
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
+
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  ;; (python-shell-interpreter "python3")
+  ;; (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+
+(use-package pyvenv
+  :after python-mode
+  :config
+  (pyvenv-mode 1))
+
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
